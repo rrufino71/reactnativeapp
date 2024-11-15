0024e1323d692629d6811c79d71ab3a7e56f0f50
rrufino71@gmail.com <rrufino71@gmail.com>
2024-11-15 10:11:23 -0300

    colores

--- screens/ContactoScreen.jsx
+++ screens/ContactoScreen.jsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function ContactoScreen() {
   return (
-    <View className="flex-1 justify-center items-center bg-customColor2-light">
+    <View className="flex-1 justify-center items-center bg-customColor1-light">
       <Text>Contacto Screen</Text>
     </View>
   );
