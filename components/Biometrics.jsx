822082d75eaa6d924e016862465b552496a41cc8
rrufino71@gmail.com <rrufino71@gmail.com>
2024-12-06 15:09:00 -0300

    bio

--- components/Biometrics.jsx
+++ components/Biometrics.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Alert, Button, Text, View } from "react-native";
 import * as LocalAuthentication from "expo-local-authentication";
+import { getSecureData, storeSecureData } from "../libs/secureStorage.js";
 
 export default function Biometrics() {
   const [isBiometricSupported, setIsBiometricSupported] = useState(false);
@@ -29,7 +30,38 @@ export default function Biometrics() {
     });
 
     if (result.success) {
-      Alert.alert("Éxito", "Autenticación completada.");
+      //Alert.alert("Éxito", "Autenticación completada.");
+
+      const storedUser = await getSecureData("user");
+      const storedPass = await getSecureData("pass");
+      console.log("storeUser:", storedUser);
+      const result = await getLogin({
+        email: storedUser,
+        password: storedPass,
+      });
+      setResponse(result);
+      if (result.status) {
+        console.log("result:", result);
+        const datosUsuario = {
+          ...result.data,
+          token: result.token,
+          mensaje: result.mensaje,
+          colorScheme: "customColor1", //agregar a la api
+        };
+        setUsuario(datosUsuario);
+        setTipoMensaje(2);
+        setMensaje(result.message);
+        setIsAuthenticated(true);
+        saveUserData("usuario", datosUsuario);
+        storeSecureData("user", form.email);
+        storeSecureData("pass", form.password);
+        navigation.replace("Home");
+      } else {
+        //Alert.alert(`Bienvenido ${result.message}, logueo fail`);
+        //console.log("error:", result.message);
+        setTipoMensaje(1);
+        setMensaje(result.message);
+      }
     } else {
       Alert.alert("Error", "La autenticación falló.");
     }
